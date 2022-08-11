f66024ce63f4803b1df4899a1633c44f232c5df5
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-08-11 08:37:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+console.log ("Receta Torta JMGD")
+console.log ("Tomar un huevo y cascarlo separando la yema y la clara en un bowl tirar la cascara a la basura")
+console.log ("Tomar un paquete de harina, abrirlo y verter el contenido en una taza hasta completar la mitad d la misma")
+console.log ("Verter el contenido d la media taza de harina en el bowl en el que estan los huevos")
+console.log ("Tomar un batidor manual y con movimientos envolventes mezclar el contenido del bowl por 3 minutos")
+console.log ("Tomar un molde de aluminio de 30 cm de diametro y rociar su cara interna con aceite en aerosol ")
+console.log ("Verter la mezcla generada en el bowl en un molde")
+console.log ("Meter el molde en un horno precalentado a 180 grados ")
+console.log ("Dejar en el horno por 40 minutos")
+console.log ("Retirar del Horno y dejar enfrir por un lapso de 20 minutos")
+console.log ("Invertir el molde sobre un plato y desmoldar")
+console.log ("Servir y comer a gusto")
+
+
+
+
